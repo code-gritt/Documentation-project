29b5115fcc21710608de838fe1baf3da3d81354d
Gokul <gokulchandan24@gmail.com>
2024-03-22 13:14:38 +0530

    made changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,8 +66,6 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "niia",
-
         items: [
           {
             type: "docSidebar",
